f23fa315fcbd9d95d9c4e3ccd1ee5d3f259a4d9e
Donald Subert <helo478@gmail.com>
2015-07-15 00:43:40 -0700

    implemented code prettify-ing on blog detail

--- public/views/blog-detail/blog-detail.js
+++ public/views/blog-detail/blog-detail.js
@@ -34,10 +34,56 @@ angular.module("codeBlog.blogDetail", ["ngRoute"])
           }).success(function(data, status, headers, config) {
             $log.debug("AJAX success for get blog detail: ", data, status, headers, config);
             $scope.blog = data;
+            populateBody();
           }).error(function(data, status, headers, config) {
             $log.error("AJAX error for get blog detail: ", data, status, headers, config);
             // TODO redirect to error page
-          })
+          });
+
+          function populateBody() {
+            var target = document.getElementById("blog-detail-body");
+            var tokens = $scope.blog.body.split("\n");
+            var newCode;
+
+            tokens.forEach(function(token) {
+
+              if (isCode(token)) {
+                // If there is not an existing code section, create one
+                if (!newCode) {
+                  newCode = document.createElement("pre");
+                  newCode.className += "prettyprint";
+                }
+                else {
+                  // Add a line break
+                  newCode.textContent += "\n";
+                }
+
+                // Add the code line
+                newCode.textContent += token.substring(3);
+              }
+              else {
+                // If the last line was code, add the code section
+                if (newCode) {
+                  target.appendChild(newCode);
+                  newCode = null;
+                }
+
+                // Add the non-code line
+                var newParagraph = document.createElement("p");
+                newParagraph.textContent = token;
+                target.appendChild(newParagraph);
+              }
+            });
+
+            // Add a final code section, if exists
+            if (newCode) { target.appendChild(newCode); }
+
+            prettyPrint();
+          }
+
+          function isCode(line) {
+            return line.startsWith("    ");
+          }
         }]
       });
 }]);
